aa2c547a2145899d5df0c02deccffc52c0231313
Linus Gasser <linus.gasser@epfl.ch>
2020-04-05 14:13:21 +0200

    moving port

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import {Server} from "ws";
 
-const wss = new Server({ port: 5555 });
+const wss = new Server({ port: 5678 });
 
-console.log("started server on port", 5555);
+console.log("started server on port", 5678);
 
 wss.on('connection', function connection(ws) {
+    console.log("connected");
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
 
     ws.send('something');
+});
+
+wss.on("error", (err)=>{
+    console.log("error:", err);
 });
